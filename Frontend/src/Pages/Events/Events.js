7347dd1539389ca59c27f283c809d91451a72710
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-02-13 15:08:40 +0300

    Added the event page

--- Frontend/src/Pages/Events/Events.js
+++ Frontend/src/Pages/Events/Events.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Carousel from "./Components/Carousel";
 import axios from "axios";
+import Countdown from "react-countdown";
+import { motion } from "framer-motion";
 import "./Events.css";
+import blogPic from "../../Assets/Images/Blog/blogs.jpg";
 
 function Events() {
-  const [activeTab, setActiveTab] = useState("completed");
-  const [upcomingEvents, setUpcomingEvents] = useState([]);
-  const [completedEvents, setCompletedEvents] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,17 +16,13 @@ function Events() {
       .get("https://ayzonfoundation.org/api/events/")
       .then((response) => {
         const events = response.data;
-        const now = new Date();
-        const upcoming = events.filter((event) => new Date(event.date) > now);
-        const completed = events.filter((event) => new Date(event.date) <= now);
-        setUpcomingEvents(upcoming);
-        setCompletedEvents(completed);
+        const sortedEvents = events.sort(
+          (a, b) => new Date(a.date) - new Date(b.date)
+        );
+        setEvents(sortedEvents);
       })
       .catch((error) => console.error("Error fetching events:", error));
   }, []);
-
-  const eventsToShow =
-    activeTab === "upcoming" ? upcomingEvents : completedEvents;
 
   const truncateText = (text, length) => {
     if (text.length > length) {
@@ -34,61 +31,175 @@ function Events() {
     return text;
   };
 
-  return (
-    <div className="App">
-      <Carousel />
-      <div className="events_head">
-        <div className="event-bg-container">
-          <div className="events_txt"> Events </div>
-        </div>
-      </div>
+  const eventsPerPage = 4;
+  const indexOfLastEvent = currentPage * eventsPerPage;
+  const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+  const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
 
-      <div className="tabs">
-        <button
-          className={`tab ${activeTab === "completed" ? "active" : ""}`}
-          onClick={() => setActiveTab("completed")}
+  const totalPages = Math.ceil(events.length / eventsPerPage);
+
+  const paginate = (pageNumber) => {
+    if (pageNumber < 1 || pageNumber > totalPages) return;
+    setCurrentPage(pageNumber);
+  };
+
+  const FlipUnit = ({ value, label }) => {
+    return (
+      <div className="flip-container">
+        <motion.div
+          className="flip-unit"
+          initial={{ rotateX: 180, opacity: 0 }}
+          animate={{ rotateX: 0, opacity: 1 }}
+          exit={{ rotateX: -180, opacity: 0 }}
+          transition={{ duration: 0.5 }}
         >
-          Completed
-        </button>
-        <button
-          className={`tab ${activeTab === "upcoming" ? "active" : ""}`}
-          onClick={() => setActiveTab("upcoming")}
-        >
-          Upcoming
-        </button>
+          {String(value).padStart(2, "0")}
+        </motion.div>
+        <span className="flip-label">{label}</span>
+      </div>
+    );
+  };
+
+  return (
+    <div className="events">
+      <div className="blog-section">
+        <img
+          src={blogPic}
+          alt="Ayzon Foundation Blog Image"
+          className="background-image-blog"
+        />
+        <h1>EVENTS</h1>
       </div>
 
       <div className="events-container">
-        {eventsToShow.map((event, index) => (
-          <div key={index} className="event-card">
-            <div className="event-date">
-              <span className="day">{new Date(event.date).getDate()}</span>
-              <span className="month">
-                {new Date(event.date)
-                  .toLocaleString("en", { month: "short" })
-                  .toUpperCase()}
-              </span>
-            </div>
-            <div
-              className="event-bg-img"
-              style={{
-                backgroundImage: `url(${event.image || "default-image.jpg"})`,
-              }}
-            ></div>
-            <div className="event-details">
-              <h2 className="event-title">{event.title}</h2>
-              <p className="event-description">
-                {truncateText(event.description, 100)}
-              </p>
-              <button
-                className="register-button"
-                onClick={() => navigate(`/event/${event.id}`)}
-              >
-                Register
-              </button>
-            </div>
+        <h1 className="next-event">Next Event</h1>
+        {currentEvents.map((event, index) => (
+          <div
+            key={index}
+            className={`event-card ${
+              index === 0 ? "full-width" : "half-width"
+            }`}
+          >
+            {index === 0 ? (
+              <div className="full-width-layout">
+                <div className="event-img-col">
+                  <div className="event-img-1">
+                    <img src={event.images[0].image} alt={event.title} />
+                  </div>
+                  <h1 className="upcoming-event">Upcoming Event</h1>
+                </div>
+                <div className="event-content">
+                  <div className="custom-countdown"></div>
+
+                  <Countdown
+                    date={new Date(`${event.date}T${event.time}Z`)}
+                    renderer={({
+                      days,
+                      hours,
+                      minutes,
+                      seconds,
+                      completed,
+                    }) => {
+                      if (completed) {
+                        return (
+                          <div className="flip-countdown completed">
+                            Event has completed
+                          </div>
+                        );
+                      }
+                      return (
+                        <div className="flip-countdown">
+                          <FlipUnit value={days} label="Days" />{" "}
+                          <p className="countdown-colon">:</p>
+                          <FlipUnit value={hours} label="Hours" />{" "}
+                          <p className="countdown-colon">:</p>
+                          <FlipUnit value={minutes} label="Minutes" />{" "}
+                          <p className="countdown-colon">:</p>
+                          <FlipUnit value={seconds} label="Seconds" />
+                        </div>
+                      );
+                    }}
+                  />
+
+                  <h2 className="event1-title">{event.title}</h2>
+                  <p className="event1-description">
+                    {truncateText(event.description, 125)}
+                  </p>
+                  <p className="event1-venue">{event.venue}</p>
+
+                  <button
+                    className="register1-button"
+                    onClick={() => navigate(`/event/${event.id}`)}
+                  >
+                    Register
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div className="half-width-layout">
+                <div className="event-img">
+                  <img src={event.images[0].image} alt={event.title} />
+                </div>
+                <div className="event-details">
+                  <h2 className="event-title">{event.title}</h2>
+                  <p className="event-description">
+                    {truncateText(event.description, 56)}
+                  </p>
+                  <p className="event-venue">{event.venue}</p>
+                  <button
+                    className="register-button"
+                    onClick={() => navigate(`/event/${event.id}`)}
+                  >
+                    Register
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         ))}
+      </div>
+
+      <div className="event-subscribe">
+        <h1 className="subscribe-title">DON'T MISS OUT ON ANY EVENTS!</h1>
+        <p className="subscribe-text">
+          Subscribe to never miss out on any events.
+        </p>
+        <div className="subscribe-box">
+          <input
+            type="email"
+            placeholder="Enter your email here"
+            className="subscribe-input"
+          />
+          <button className="subscribe-button">Subscribe</button>
+        </div>
+      </div>
+
+      <div className="events-pagination">
+        <button
+          className="events-page-button"
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i + 1}
+            onClick={() => paginate(i + 1)}
+            className={`events-page-button ${
+              currentPage === i + 1 ? "active" : ""
+            }`}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button
+          className="events-page-button"
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
